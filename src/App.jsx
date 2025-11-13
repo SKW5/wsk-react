92790a0de8711e488fab0e44b322a5ebe33eca47
aleksi <sdf@>
2025-11-13 13:46:24 +0200

    first-component actually

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Home from './components/Home.jsx'
+const App = () => {
+    return (
+        <>
+            <h1>My app</h1>
+            <Home />
+        </>
+    )
+}
+export default App;
