4aac5f637875670ed6b0bd8d9ee21043dbe8300f
SKW5 <78236165+SKW5@users.noreply.github.com>
2025-11-27 00:10:53 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Upload from './views/Upload.jsx'
 
 const App = () => {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
             <Routes>
                 <Route element={<Layout />}>
                 <Route path="/" element={<Home />} />
